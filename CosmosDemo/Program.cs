daee30a4b9f9c714f61a8c17effc1d1a9ac0c444
Jianjun Lv <jianjlv@microsoft.com>
2019-07-11 10:49:46 +0800

    remove useless comment

--- CosmosDemo/Program.cs
+++ CosmosDemo/Program.cs
@@ -9,7 +9,6 @@
         static void Main(string[] args)
         {
             // You can get the sample stram path and thumbprint from the doc: https://microsoftapc-my.sharepoint.com/personal/jianjlv_microsoft_com/_layouts/OneNote.aspx?id=%2Fpersonal%2Fjianjlv_microsoft_com%2FDocuments%2FJianjun%20%40%20Microsoft&wd=target%28Work.one%7C08C13A75-D69C-49FE-8D53-8DBF6710CCF0%2FSample%20Code%7CF29C765D-F05A-4516-8F35-08DCE5847D4C%2F%29
-            //str/ing stream = "https://cosmos14.osdinfra.net/cosmos/CFR.prod/local/Dimensions/EmailStorageMailboxRange/EmailStorageMailbox_007D_2019_06_19.ss";
 
             string stream = "";
 
