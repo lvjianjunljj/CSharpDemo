e606d2654b685fd975c24c8e3982a0291b8045e3
Jianjun Lv <jianjlv@microsoft.com>
2019-09-06 00:21:32 +0800

    update json file operation

--- CSharpDemo/IDEAs/DatasetJsonFileOperation.cs
+++ CSharpDemo/IDEAs/DatasetJsonFileOperation.cs
@@ -1,8 +1,14 @@
 ﻿namespace CSharpDemo.IDEAs
 {
+    using CSharpDemo.FileOperation;
+    using Newtonsoft.Json.Linq;
+    using System;
+
     class DatasetJsonFileOperation
     {
-        static string folderPath = "";
+        private static string FolderPath = @"D:\IDEAs\Ibiza\Source\DataCopMonitors\PROD";
+
+
         public static void MainMethod()
         {
             UpdateDatasetJsonForMergingADLSCosmos();
@@ -10,7 +16,33 @@
 
         public static void UpdateDatasetJsonForMergingADLSCosmos()
         {
+            var folders = ReadFile.GetFolderSubPaths(FolderPath, ReadType.Directory, PathType.Absolute);
+            foreach (var folder in folders)
+            {
+                System.Console.WriteLine(folder);
+                string subFolderPath = folder + @"\Datasets";
+                try
+                {
+                    var datasetJsonFiles = ReadFile.GetFolderSubPaths(subFolderPath, ReadType.File, PathType.Absolute);
+                    foreach (var filePath in datasetJsonFiles)
+                    {
+                        //System.Console.WriteLine(filePath);
+                        string datasetString = ReadFile.ThirdMethod(filePath);
+                        JObject datasetJObject = JObject.Parse(datasetString);
+                        //Console.WriteLine(datasetJObject["dataFabric"]);
+                        if (datasetJObject["dataFabric"].ToString() == "ADLS")
+                        {
+                            // Do something
+                            // I don't write the code because there is not ADLS dataset in the folder
+                        }
+                    }
 
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
