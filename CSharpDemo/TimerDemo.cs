d7e7b8561e9dc67f8debbd0c42ad89296dffb22b
Jianjun Lv <jianjlv@microsoft.com>
2019-12-11 11:39:00 +0800

    Update timer demo code and add a new empty project SQLDemo

--- CSharpDemo/TimerDemo.cs
+++ CSharpDemo/TimerDemo.cs
@@ -9,12 +9,13 @@ namespace CSharpDemo
         public static void MainMethod()
         {
             //TestSyncMethod();
-            TestAsyncMethod();
+            //TestAsyncMethod();
+            ZeroSecondTrigger();
         }
 
         public static void TestSyncMethod()
         {
-            Console.WriteLine($"Sync Method Test. Time: {DateTime.Now}");
+            Console.WriteLine($"Sync Method Test. Time: {DateTime.UtcNow:o}");
             TimerDemo t = new TimerDemo();
             Timer serviceHeartbeatTimer = new Timer(t.TestSync);
             // The two input is the duration between start time and now, the period.
@@ -23,7 +24,7 @@ namespace CSharpDemo
 
         public static void TestAsyncMethod()
         {
-            Console.WriteLine($"Async Method Test. Time: {DateTime.Now}");
+            Console.WriteLine($"Async Method Test. Time: {DateTime.UtcNow:o}");
             TimerDemo t = new TimerDemo();
             Timer serviceHeartbeatTimer = new Timer(t.TestAsync);
             // The two input is the duration between start time and now, the period.
@@ -32,9 +33,9 @@ namespace CSharpDemo
 
         public void TestSync(object state)
         {
-            Console.WriteLine($"Start Test. Time: {DateTime.Now}");
+            Console.WriteLine($"Start Test. Time: {DateTime.UtcNow:o}");
             Thread.Sleep(3000);
-            Console.WriteLine($"End Test. Time: {DateTime.Now}");
+            Console.WriteLine($"End Test. Time: {DateTime.UtcNow:o}");
 
             // We can stop the time in the call function with state.
             ((Timer)state).Dispose();
@@ -43,13 +44,30 @@ namespace CSharpDemo
 
         public async void TestAsync(object state)
         {
-            Console.WriteLine($"Start TestAsync. Time: {DateTime.Now}");
+            Console.WriteLine($"Start TestAsync. Time: {DateTime.UtcNow:o}");
             await Task.Delay(TimeSpan.FromSeconds(3));
-            Console.WriteLine($"End TestAsync. Time: {DateTime.Now}");
+            Console.WriteLine($"End TestAsync. Time: {DateTime.UtcNow:o}");
 
             // We can stop the time in the call function with state.
             ((Timer)state).Dispose();
             Console.WriteLine("Stop the TestAsync...");
+        }
+
+        public static void ZeroSecondTrigger()
+        {
+            int minutes = 2;
+            Console.WriteLine($"Async Method Test. Time: {DateTime.UtcNow:o}");
+            TimerDemo t = new TimerDemo();
+            Timer serviceHeartbeatTimer = new Timer(t.ZeroSecondTrigger);
+            // Trigger the function in the beginning of every few mins
+            serviceHeartbeatTimer.Change(TimeSpan.FromMilliseconds((minutes - DateTime.Now.Minute % minutes) * 60 * 1000 - DateTime.Now.Second * 1000 - DateTime.UtcNow.Millisecond), TimeSpan.FromMinutes(minutes));
+        }
+
+        public async void ZeroSecondTrigger(object state)
+        {
+            Console.WriteLine($"Start TestAsync. Time: {DateTime.UtcNow:o}");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine($"End TestAsync. Time: {DateTime.UtcNow:o}");
         }
     }
 }
---
+++ SQLDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SQLDemo
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1234);
+
+            Console.ReadKey();
+        }
+    }
+}
