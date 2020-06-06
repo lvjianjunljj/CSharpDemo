9a2bee36d08325fc1f32798f8ab16a7d910741ad
Jianjun Lv <jianjlv@microsoft.com>
2020-06-06 18:11:49 +0800

    Add new project Adla2 to test Adla sdk with .Net Framework 4.6.1

---
+++ AdlaDemo2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AdlaDemo2
+{
+    // The new project for testing ADLA SDK with .Net Framework 4.6.1
+    // The test with .Net Framework 4.8.0 failed.
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
