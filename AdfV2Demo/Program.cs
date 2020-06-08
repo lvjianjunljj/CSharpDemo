75dda25e2ffc013dc5cfb7f8c1efa3c68502100b
Jianjun Lv <jianjlv@microsoft.com>
2020-06-08 19:24:25 +0800

    Add new project for ADF v2 test

--- AdfV2Demo/Program.cs
+++ AdfV2Demo/Program.cs
@@ -1,19 +1,16 @@
-﻿// Sample code for testing ADF v1 sdk: Microsoft.Azure.Management.DataFactories
-namespace AdfDemo
+﻿namespace AdfV2Demo
 {
-    using System;
-    using Microsoft.Azure;
-    using Microsoft.Azure.Management.DataFactories;
-    using Microsoft.Azure.Management.DataFactories.Models;
+    using Microsoft.Azure.Management.DataFactory;
     using Microsoft.IdentityModel.Clients.ActiveDirectory;
+    using Microsoft.Rest;
     using Newtonsoft.Json;
+    using System;
 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Start...");
-
             string tenant_id = "";
             string client_id = "";
             string client_key = "";
@@ -22,28 +19,16 @@ namespace AdfDemo
             string dataFactory = "";
             string pipelineName = "";
             string datasetName = "";
-            string linkedServiceName = "";
-
-            /*
-             * Sample v1 azure data factory in ideas-ppe
-             * ADF v1 service will be shut down for Microsoft internal customers on June 30, 2020.
-             * You won’t be able to create ADF v1 data factories, pipelines after June 30, 2020.
-             * The existing ADF v1 pipelines will automatically shut down and you will not be able to view,
-             * execute your existing ADF v1 pipelines. Migrate your existing ADF v1 pipelines to ADF v2
-             */
-            dataFactory = "ideas-ppe-adf-sandbox";
-            pipelineName = "CosmosToBlob_UserTypeDim";
-            datasetName = "Cosmos_UserTypeDim";
-            linkedServiceName = "CosmosLinkedService";
+            //string linkedServiceName = "";
 
             /*
              * Sample v2 azure data factory in ideas-ppe
              * Microsoft.Azure.Management.DataFactories is the sdk for ADF v1,
              * we cannot use it to find the Data factory v2.
              */
-            //dataFactory = "fcm-adf";
-            //pipelineName = "invokeBricks";
-            //datasetName = "blobIn";
+            dataFactory = "fcm-adf";
+            pipelineName = "invokeBricks";
+            datasetName = "blobIn";
 
             // Not sure if there is linked service list in ADF v2
             //linkedServiceName = "CosmosLinkedService";
@@ -62,8 +47,11 @@ namespace AdfDemo
 
             var token = result.AccessToken;
 
-            var _credentials = new TokenCloudCredentials(subscription_id, token);
-            var client = new DataFactoryManagementClient(_credentials);
+            ServiceClientCredentials _credentials = new TokenCredentials(token);
+            var client = new DataFactoryManagementClient(_credentials)
+            {
+                SubscriptionId = subscription_id
+            };
 
             var pipeline = client.Pipelines.Get(resourceGroup, dataFactory, pipelineName);
             //pipeline.Pipeline.Properties.Start = DateTime.Parse($"{slice.Date:yyyy-MM-dd}T00:00:00Z");
@@ -75,11 +63,11 @@ namespace AdfDemo
             //});
 
             var dataset = client.Datasets.Get(resourceGroup, dataFactory, datasetName);
-            var linkedService = client.LinkedServices.Get(resourceGroup, dataFactory, linkedServiceName);
+            //var linkedService = client.LinkedServices.Get(resourceGroup, dataFactory, linkedServiceName);
 
             Console.WriteLine(JsonConvert.SerializeObject(pipeline));
             Console.WriteLine(JsonConvert.SerializeObject(dataset));
-            Console.WriteLine(JsonConvert.SerializeObject(linkedService));
+            //Console.WriteLine(JsonConvert.SerializeObject(linkedService));
 
             //Pipeline pipelineInput = new Pipeline
             //{
