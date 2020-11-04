f83e438c7123431337366869a43a8870b008f992
Jianjun Lv <jianjlv@microsoft.com>
2020-11-04 21:17:56 +0800

    Add getting secrets from local file logic

--- AzureLib/KeyVault/KeyVaultSecretProvider.cs
+++ AzureLib/KeyVault/KeyVaultSecretProvider.cs
@@ -1,14 +1,21 @@
 ﻿namespace AzureLib.KeyVault
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
 
     using Microsoft.Azure.KeyVault;
     using Microsoft.Azure.KeyVault.Models;
     using Microsoft.Azure.Services.AppAuthentication;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     public class KeyVaultSecretProvider : ISecretProvider
     {
+        private const string JsonFilePath = @"D:\data\company_work\M365\IDEAs\secrets.json";
+
         private static Lazy<KeyVaultSecretProvider> secretProvider = new Lazy<KeyVaultSecretProvider>(() => new KeyVaultSecretProvider());
 
         private KeyVaultSecretProvider()
@@ -26,11 +33,45 @@
         /// <returns></returns>
         public async Task<string> GetSecretAsync(string keyVaultName, string secretName)
         {
-            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
-            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
-            string vaultUri = $"https://{keyVaultName}.vault.azure.net/";
-            SecretBundle secret = await keyVaultClient.GetSecretAsync(vaultUri, secretName);
-            return secret.Value;
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                string vaultUri = $"https://{keyVaultName}.vault.azure.net/";
+                SecretBundle secret = await keyVaultClient.GetSecretAsync(vaultUri, secretName);
+                return secret.Value;
+            }
+            catch (AzureServiceTokenProviderException ae)
+            {
+                if (ae.Message.Contains("[No connection string specified]"))
+                {
+
+                    return GetSecretByJson(keyVaultName, secretName);
+                }
+
+                throw;
+            }
+        }
+
+        private string GetSecretByJson(string keyVaultName, string secretName)
+        {
+            string jsonContent = File.ReadAllText(JsonFilePath, Encoding.UTF8);
+            var jarray = JArray.Parse(jsonContent);
+            foreach (var keyVaultJson in jarray)
+            {
+                if (keyVaultJson["keyVaultName"].ToString().Equals(keyVaultName))
+                {
+                    foreach (var secretJson in keyVaultJson["secrets"].ToArray())
+                    {
+                        if (secretJson["secretName"].ToString().Equals(secretName))
+                        {
+                            return secretJson["secretValue"].ToString();
+                        }
+                    }
+                }
+            }
+
+            throw new Exception($"Cannot get the secret '{keyVaultName} - {secretName}' from local json file, please update the file.");
         }
     }
 }
