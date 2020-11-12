e98aa92dedda90d94a91a550ba77898d7519c27e
Jianjun Lv <jianjlv@microsoft.com>
2020-11-12 22:27:06 +0800

    Update the logic of app config update

--- CSharpDemo/FileOperation/AppConfigOperation.cs
+++ CSharpDemo/FileOperation/AppConfigOperation.cs
@@ -2,17 +2,54 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
     using System.Xml;
 
     public class AppConfigOperation
     {
-        private static string FolderPath = @"D:\IDEAs\repos\Ibiza\Source\Service\DataCop";
+        private static string FolderPath = @"D:\IDEAs\repos\Ibiza\Source\Services\DataCop";
         public static void MainMethod()
         {
-            UpdateKeyValuePair(null, null);
+            UpdateToDataCopTorus();
+        }
+        public static void UpdateToDataCopTorus()
+        {
+            var pairs = new Dictionary<string, string>
+            {
+                ["KeyVaultName"] = "datacop-ppe",
+                ["Environment"] = "ppe",
+                ["TenantId"] = "cdc5aeea-15c5-4db6-b079-fcadd2505dc2",
+            };
+            var configFilePaths = Directory.GetFiles(FolderPath, "*pp.config", SearchOption.AllDirectories);
+            foreach (var configFilePath in configFilePaths)
+            {
+                Console.WriteLine(configFilePath);
+                UpdateKeyValuePair(configFilePath, pairs);
+            }
         }
 
-        private static void UpdateKeyValuePair(string filePath, Dictionary<string, string> pairs)
+        private static void UpdateKeyValuePair(string configFilePath, Dictionary<string, string> pairs)
+        {
+            var content = File.ReadAllText(configFilePath, Encoding.UTF8);
+            foreach (var key in pairs.Keys)
+            {
+                var queryContent = $"<add key=\"{key}\" value=\"";
+                var index = content.IndexOf(queryContent);
+                while (index >= 0)
+                {
+                    var start = index + queryContent.Length;
+                    var end = content.IndexOf("\"", start);
+                    content = content.Substring(0, start) + pairs[key] + content.Substring(end);
+                    index = content.IndexOf(queryContent, end);
+                }
+            }
+
+            WriteFile.FirstMethod(configFilePath, content);
+        }
+
+        // Not work as I expect
+        private static void DemoFunction()
         {
             XmlTextReader xmlReader = new XmlTextReader(
                 @"D:\IDEAs\repos\Ibiza\Source\Services\DataCop\Common\app.config");
@@ -30,7 +67,7 @@
                 //    System.Console.WriteLine("");
                 //    break;
                 //}
-                Console.WriteLine($"{xmlReader.Name}: {xmlReader.}");
+                Console.WriteLine($"{xmlReader.Name}: {xmlReader.Value}");
             }
         }
     }
